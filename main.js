5dd588f54e002019d77eab98adf8556527f20ece
miogy <jjangrl87@gmail.com>
2022-12-05 23:50:28 +0900

    ADD : js-project category filter

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ const navbar = document.querySelector("#navbar");
 const navbarHeight = navbar.getBoundingClientRect().height;
 
 document.addEventListener("scroll", () => {
-  console.log(window.scrollY); // y 확인
-  console.log(`navbarHeight:${navbarHeight}`); // y 확인
+  //console.log(window.scrollY); // y 확인
+  //console.log(`navbarHeight:${navbarHeight}`); // y 확인
 
   // if (window.scrollY > navbarHeight) {
   //   navbar.classList.add("navbar--dark");
@@ -43,6 +43,43 @@ asideTopBtn.addEventListener("click", () => {
 const asideBottomBtn = document.querySelector(".aside__bottom");
 asideBottomBtn.addEventListener("click", () => {
   scrollIntoView("#contact");
+});
+
+// scroll down => home fade
+
+const home = document.querySelector(".home__container");
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener("scroll", () => {
+  //  console.log(1 - window.scrollY / homeHeight); //scroll계산
+  home.style.opacity = 1 - window.scrollY / homeHeight;
+  home.style.transition = "all 500ms ease";
+});
+
+//category => filtering
+
+const workBtnContainer = document.querySelector(".work__categories");
+const projectContainer = document.querySelector(".work__projects");
+//각각의 컨테이너 안에 프로젝트들을 받아옴
+const projects = document.querySelectorAll(".project");
+//btn click
+workBtnContainer.addEventListener("click", (e) => {
+  const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter; //parentNode를 사용하여 target지정(span)
+  //console.log(filter); //e.target값 지정
+  if (filter == null) {
+    return;
+  }
+  // let project;
+  // for (let i = 0; i < projects.length; i++) {
+  //   project = projects[i];
+  // }
+  projects.forEach((project) => {
+    // for문과 같은 의미, 배열을 불러와서 정렬
+    if (filter === "*" || filter === project.dataset.type) {
+      project.classList.remove("invisible");
+    } else {
+      project.classList.add("invisible");
+    }
+  });
 });
 
 // 중복코드시 함수로 만들어서 사용
